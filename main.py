81e7966f6174dce2976ee1759cd97db4b634fa4f
SuiJin Foong <foongsj@gmail.com>
2022-02-23 16:00:53 +0800

    Added argparse to allow new log file name

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 from time import sleep
+import argparse
 
-f = open('test.log')
+parser = argparse.ArgumentParser()
+parser.add_argument("input", default="test.log", nargs="?",
+    help="Input syslog file. Default: test.log")
+args = parser.parse_args()
+
+f = open(args.input)
 s = ' '
 line = f.readline()
 
 while(1): # Loop forever
   while(line):
-    elements = line.split(' ')
+    elements = line.split(s)
     if (len(elements)>1): # Ensure line is actually a log (not an empty line)
       if (elements[10] == 'created'):
         print(s.join(elements[:16]), elements[27]) # Grab first 17 elements, followed by 28th
